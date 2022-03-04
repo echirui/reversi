7e75de5a4be0a2398a76fe9a0a27487fbca3bce1
Rui Echizenya <rui.echizenya@gmail.com>
2022-03-04 16:52:58 +0900

    通常フォーマットの棋譜を読み込めるように変更。f5d6c3...

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # reversi
-create reversi game
+##reversi game を作成してAIの勉強をしてみる
+1. 目標
+	1. 機能
+		普通のルール通りに動くこと
+	2. AI
+		自分が勝てなくなるくらい
--- reversi.py
+++ reversi.py
@@ -10,6 +10,7 @@ B = 1    # 2bit 0b01
 W = 2    # 2bit 0b10
 MASK = 0b11 # and enable flag 2bit 0b11
 
+CONVERT_DICT = {'a':'0','b':'1','c':'2','d':'3','e':'4','f':'5','g':'6','h':'7'}
 # [R, D, L, U, RU, RD, LD, LU]
 list_direct_x = [1, 0, -1, 0, 1, 1, -1, -1]
 list_direct_y = [0, 1, 0, -1, -1, 1, 1, -1]
@@ -80,7 +81,7 @@ class Reversi:
         return
 
     def put(self, x, y):
-        """ put stone. And check illegal
+        """ put stone. And check illegal.j
         """
         # check illegal move
         if not self.is_inside(x, y):
@@ -94,10 +95,11 @@ class Reversi:
             return False
         # put stone
         self.board[x][y] = self.player
+        self.kifu.append(str(x)+str(y))
         self.num_stones[self.player] += 1
         return True
 
-    def next_turn(self):
+    def turn_next(self):
         """
         """
         # 01 xor 11 = 10, 10 xor 11 = 01
@@ -162,8 +164,12 @@ class Reversi:
         elif s[B] < s[W]:
             return "White won!"
 
-    def convert_kifu(self):
-        pass
+    @staticmethod
+    def convert_kifu(ki):
+        list_return = list()
+        for i in range(0, len(ki), 2):
+            list_return.append([CONVERT_DICT[ki[i]], str(int(ki[i+1])-1)])
+        return list_return
 
     def print_score(self):
         print(f"Score[B:{self.num_stones[B]}, W:{self.num_stones[W]}]")
@@ -205,9 +211,9 @@ class Reversi:
 # static fucntion
 is_inside = Reversi.is_inside
 is_empty = Reversi.is_empty
+convert_kifu = Reversi.convert_kifu
 
-
-def game(test_input=None):
+def game(kifu_input=None):
     ''' start Reversi
     '''
     rv = Reversi()
@@ -217,16 +223,17 @@ def game(test_input=None):
     check       = rv.check
     check_pass  = rv.check_pass
     clr_suggest = rv.clear_suggest
-    next_turn   = rv.next_turn
+    turn_next   = rv.turn_next
     end         = rv.end
     input_move  = rv.input_move
     output      = rv.output
     msg         = rv.message
 
-    pre_input = None
-    if test_input:
-        pre_input = test_input.split(',')
-        pre_input = [[x[0], x[1]] for x in pre_input]
+    if kifu_input:
+        if kifu_input[0] in CONVERT_DICT.keys():
+            kifu_input = convert_kifu(kifu_input)
+        else:
+            kifu_input = [[x[0], x[1]] for x in kifu_input.split(',')]
 
     # main loop
     while not end():
@@ -234,32 +241,31 @@ def game(test_input=None):
             rv.pass_flag = 0
         else:
             rv.pass_flag += 1
-            next_turn()
+            turn_next()
             continue
 
-        if pre_input:
-            x, y = map(int, pre_input.pop(0))
+        if kifu_input:
+            x, y = map(int, kifu_input.pop(0))
         else:
             output()
             x, y = input_move()
         if not put(x, y):
             continue
-        rv.kifu.append(str(x)+str(y))
         clr_suggest()
-        # check 8 direction. and turn stone up side down.
+        # check 8 direction. and turn the stone upside down.
         for dx, dy in list_direct:
             length = check(x, y, dx, dy)
             if length:
                 flip(x, y, dx, dy, length)
-        next_turn()
+        turn_next()
     else:
         output()
         msg(0, f"{rv.win_reason()} Final Score Black:{rv.num_stones[B]} White:{rv.num_stones[W]}")
-        #print('Repeat game(\'{}\')'.format(','.join(rv.kifu)))
+        print('Repeat game(\'{}\')'.format(','.join(rv.kifu)))
 
 def test_game(test=None):
     '''
-    >>> game('45,35,22,54,65,56,36,74,76,46,47,55')
+    >>> game('45,35,22,54,65,56,36,74,76,46,47')
     Score[B:14, W:1]
         0 1 2 3 4 5 6 7
     00  . . . . . . . .
@@ -271,6 +277,7 @@ def test_game(test=None):
     60  . . . . . ○ . .
     70  . . . . ● . ○ .
     Black won! Final Score Black:14 White:1
+    Repeat game('45,35,22,54,65,56,36,74,76,46,47')
     >>> game('45,53,42,35,24,55,46,54,64')
     Score[B:13, W:0]
         0 1 2 3 4 5 6 7
@@ -283,6 +290,7 @@ def test_game(test=None):
     60  . . . . ○ . . .
     70  . . . . . . . .
     Black won! Final Score Black:13 White:0
+    Repeat game('45,53,42,35,24,55,46,54,64')
     >>> game('45,35,26,36,46,17,32,53,63,62,61,71,16,15,04,22,12,11,10,00,23,02,01,20,21,51,60,31,30,40,41,03,06,05,13,14,24,25,07,50,27,70,55,37,47,57,67,56,66,76,42,72,54,73,77,65,64,75,74')
     Score[B:26, W:37]
         0 1 2 3 4 5 6 7
@@ -295,6 +303,33 @@ def test_game(test=None):
     60  ● ● ● ● ○ ○ ○ ○
     70  ● ● ● ● ○ ○ ○ ○
     White won! Final Score Black:26 White:37
+    Repeat game('45,35,26,36,46,17,32,53,63,62,61,71,16,15,04,22,12,11,10,00,23,02,01,20,21,51,60,31,30,40,41,03,06,05,13,14,24,25,07,50,27,70,55,37,47,57,67,56,66,76,42,72,54,73,77,65,64,75,74')
+    >>> game('f5d6c3d3c6f4e3c5f6e6e7f3c4e2f1d1g4g3d7h4h2g5h5h3d2h1g2h6f2b3b4g1a3e1c2c1g6h7h8b1f7f8a5b5a6b6g7g8e8d8c8b8c7a7b7a8b2a1a2a4')
+    Score[B:12, W:46]
+        0 1 2 3 4 5 6 7
+    00  . . ○ . ● ● ● .
+    10  ● . ○ ○ ○ ● . ●
+    20  ● ● ● ● ● ○ ○ ●
+    30  ● ● ● ● ● ○ ○ ●
+    40  ● ● ● ● ○ ○ ● ●
+    50  ● ● ● ○ ○ ● ● ●
+    60  ● ● ● ● ● ● ● ●
+    70  ● ● ● ● ● ● ● ●
+    White won! Final Score Black:12 White:46
+    Repeat game('54,35,22,32,25,53,42,24,55,45,46,52,23,41,50,30,63,62,36,73,71,64,74,72,31,70,61,75,51,12,13,60,02,40,21,20,65,76,77,10,56,57,04,14,05,15,66,67,47,37,27,17,26,06')
+    >>> game('f5f6e6f4g5e7f7g6h5d7d6h6h7c6h4c7g4f8d8e8g8h3h2g7c5b6h8c4b3c3c8a3g3g2g1b8a6b7b5b4a8a7a5a4a2b2')
+    Score[B:50, W:0]
+        0 1 2 3 4 5 6 7
+    00  . ○ ○ ○ ○ ○ ○ ○
+    10  . ○ ○ ○ ○ ○ ○ ○
+    20  . . ○ ○ ○ ○ ○ ○
+    30  . . . ○ ○ ○ ○ ○
+    40  . . . ○ ○ ○ ○ ○
+    50  . . . ○ ○ ○ ○ ○
+    60  ○ ○ ○ ○ ○ ○ ○ ○
+    70  . ○ ○ ○ ○ ○ ○ ○
+    Black won! Final Score Black:50 White:0
+    Repeat game('54,55,45,53,64,46,56,65,74,36,35,75,76,25,73,26,63,57,37,47,67,72,71,66,24,15,77,23,12,22,27,02,62,61,60,17,05,16,14,13,07,06,04,03,01,11')
     '''
     pass
 
